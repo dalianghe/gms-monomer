0f1d09efc263b2d21d1ab04eb84147d084d4f902
dalianghe <276420284@qq.com>
2018-01-14 20:18:45 +0800

    提交代码

---
+++ gms-generator/src/main/java/com/github/dalianghe/admin/user/mapper/UserMapper.java
@@ -0,0 +1,7 @@
+package com.github.dalianghe.admin.user.mapper;
+
+import com.github.dalianghe.admin.user.entity.User;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface UserMapper extends Mapper<User> {
+}
