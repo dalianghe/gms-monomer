a45b7f326852193a415008dec2dea603d9d64b17
dalianghe <276420284@qq.com>
2018-01-17 23:26:56 +0800

    提交代码

--- gms-ui/src/main/java/com/github/dalianghe/ui/controller/menu/MenuController.java
+++
@@ -1,7 +0,0 @@
-package com.github.dalianghe.ui.controller.menu;
-
-/**
- * Created by admin on 2018/1/12.
- */
-public class MenuController {
-}
--- gms-ui/src/main/java/com/github/dalianghe/ui/controller/user/UserController.java
+++
@@ -1,7 +0,0 @@
-package com.github.dalianghe.ui.controller.user;
-
-/**
- * Created by admin on 2018/1/12.
- */
-public class UserController {
-}
